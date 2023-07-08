faf4536df12077facf8a2f195bb351e36e974935
empriego <em.priego@gmail.com>
2023-07-09 00:06:38 +0100

    update app palette

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -26,7 +26,7 @@ export default function WeatherIcon(props) {
   return (
     <ReactAnimatedWeather
       icon={codeMapping[props.code]}
-      color="#F3F3F6"
+      color="#d6d5ff"
       size={props.size}
       animate={true}
     />
